79c58a79bc836da3f31690d942006a9249941bb6
Franck <franck.spadotto@yahoo.com>
2025-08-20 15:04:24 +0200

    Check collision

--- main.c
+++ main.c
@@ -1,26 +1,45 @@
 #include "main.h"
 
 /*
- *Largeur = 160px -> 20 tiles
- *Longeur = 144px -> 18 tiles
+ * Largeur = 160px -> 20 tiles
+ * Longeur = 144px -> 18 tiles
  *
- *Haut Gauche	-> 8, 16
- *Haut Droit	-> 160, 16
- *Bas Droit	-> 160, 152
- *Bas Gauche	-> 8, 152
+ * Haut Gauche	-> 8, 16
+ * Haut Droit	-> 160, 16
+ * Bas Droit	-> 160, 152
+ * Bas Gauche	-> 8, 152
+ */
+
+/*
+ * 0b00000000, 0b00000000 -> bit bas, bit haut -> ligne blanche
+ * 00 -> bit haut = 0, bit bas = 0 -> blanc
+ * 01 -> bit haut = 0, bit bas = 1 -> gris clair
+ * 10 -> bit haut = 1, bit bas = 0 -> gris foncé
+ * 11 -> bit haut = 1, bit bas = 1 -> noir
  */
 
 void main(void)
 {
 	unsigned char perso[] = {
-		0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF
+		0b11111111, 0b11111111,
+		0b11111111, 0b11111111,
+		0b11111111, 0b11111111,
+		0b11111111, 0b11111111,
+		0b11111111, 0b11111111,
+		0b11111111, 0b11111111,
+		0b11111111, 0b11111111,
+		0b11111111, 0b11111111,
+	};
+
+	unsigned char pixel_sprite[] = {
+		0b10000000, 0b10000000,
+		0b00000000, 0b00000000,
+		0b00000000, 0b00000000,
+		0b00000000, 0b00000000,
+		0b00000000, 0b00000000,
+		0b00000000, 0b00000000,
+		0b00000000, 0b00000000,
+		0b00000000, 0b00000000,
 	};
 
 	UINT8 x = 8; /* Position X initiale */
@@ -30,7 +49,12 @@ void main(void)
 	set_sprite_data(0, 1, perso);	/* Charge tile 0 */
 	set_sprite_tile(0, 0);	/* Sprite 0 utilise tile 0 */
 	move_sprite(0, x, y);	/* Position initiale */
-	SHOW_SPRITES;		/* Active les sprites */
+
+	set_sprite_data(1, 1, pixel_sprite);
+	set_sprite_tile(1, 1); /* sprite 1 utilise tile 1 */
+	move_sprite(1, 80, 72); /* milieu de l'écran */
+
+	SHOW_SPRITES;
 
 	/* Boucle principale */
 	while (1)
--- movement.c
+++ movement.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * movement - Handle player movement
+ * player_movement - Handle player movement
  * @x: X player position
  * @y: Y pleyer position
  */
 void player_movement(UINT8 *x, UINT8 *y)
 {
-	if (joypad() & J_LEFT)
+	if (joypad() & J_LEFT && *x > 8)
 		(*x)--;
-	if (joypad() & J_RIGHT)
+	if (joypad() & J_RIGHT && *x < 160)
 		(*x)++;
-	if (joypad() & J_UP)
+	if (joypad() & J_UP && *y > 16)
 		(*y)--;
-	if (joypad() & J_DOWN)
+	if (joypad() & J_DOWN && *y < 152)
 		(*y)++;
 
 	move_sprite(0, *x, *y);
