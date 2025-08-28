d934b86d0322b252207f2cd8b0fb26abc5d990c6
Franck <franck.spadotto@yahoo.com>
2025-08-28 11:56:53 +0200

    Manage world sprites in 1 file

--- world.c
+++ world.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include "assets/world/sprite_wall.c"
-#include "assets/world/sprite_white.c"
+#include "assets/world/main_bg.c"
 #include "assets/world/world_map.c"
 
 #define SCREEN_X 160
@@ -10,7 +9,6 @@
 #define PLAYER_X 8
 #define PLAYER_Y 8
 
-
 INT8 cam_x = 0;
 INT8 cam_y = 0;
 
@@ -18,17 +16,16 @@ void world_init(void)
 {
 	int i, j;
 
-	set_bkg_data(0, 1, sprite_white);
-	set_bkg_data(1, 1, sprite_wall);
+	set_bkg_data(0, 3, main_bg);
 
 	for (i = 0; i < MAP_HEIGHT; i++)
 	{
 		for (j = 0; j < MAP_WIDTH; j++)
 		{
 			if (map[i][j])
-				map[i][j] = 1;
+				map[i][j] = 2;
 			else
-				map[i][j] = 0;
+				map[i][j] = 1;
 		}
 	}
 
@@ -58,32 +55,32 @@ void world_scroll(void)
 	{
 		player_direction = DIR_LEFT;
 		next_left = (cam_x + SCREEN_X / 2 - speed) / TILE;
-		if (map[player_tile_y_up][next_left] == 0 &&
-		   map[player_tile_y_down][next_left] == 0)
+		if (map[player_tile_y_up][next_left] == 1 &&
+		   map[player_tile_y_down][next_left] == 1)
 			cam_speed_x = -speed;
 	}
 	if (joypad() & J_RIGHT)
 	{
 		player_direction = DIR_RIGHT;
 		next_right = (cam_x + SCREEN_X / 2 + PLAYER_X - 1 + speed) / TILE;
-		if (map[player_tile_y_up][next_right] == 0 &&
-		   map[player_tile_y_down][next_right] == 0)
+		if (map[player_tile_y_up][next_right] == 1 &&
+		   map[player_tile_y_down][next_right] == 1)
 			cam_speed_x = speed;
 	}
 	if (joypad() & J_UP)
 	{
 		player_direction = DIR_UP;
 		next_up = (cam_y + SCREEN_Y / 2 - speed) / TILE;
-		if (map[next_up][player_tile_x_left] == 0 &&
-		   map[next_up][player_tile_x_right] == 0)
+		if (map[next_up][player_tile_x_left] == 1 &&
+		   map[next_up][player_tile_x_right] == 1)
 			cam_speed_y = -speed;
 	}
 	if (joypad() & J_DOWN)
 	{
 		player_direction = DIR_DOWN;
 		next_down = (cam_y + SCREEN_Y / 2 + PLAYER_Y - 1 + speed) / TILE;
-		if (map[next_down][player_tile_x_left] == 0 &&
-		   map[next_down][player_tile_x_right] == 0)
+		if (map[next_down][player_tile_x_left] == 1 &&
+		   map[next_down][player_tile_x_right] == 1)
 			cam_speed_y = speed;
 	}
 
